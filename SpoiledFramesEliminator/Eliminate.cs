699dafe86a6464834654be51050943ac2085cda7
Andrey Simukov <Andrey>
2018-10-17 20:34:26 +0300

    initial project version

---
+++ SpoiledFramesEliminator/Eliminate.cs
@@ -0,0 +1,9 @@
+﻿namespace SpoiledFramesEliminator
+{   
+    //takes a list of jpg and raw files, generates a list of raw files to remove, removes them
+    public class Eliminate
+    {   
+        
+        
+    }
+}
---
+++ SpoiledFramesEliminator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace SpoiledFramesEliminator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        //take parameter(s)
+	        //var pathParameter = args[0];
+	        var pathParameter = @".\..\TestFolder";
+
+			//call something to get the list of JPEG and RAW files
+			Search searching = new Search(pathParameter);
+
+	        //call smthng to remove RAW files
+
+			//write a message
+
+	        
+        }
+	    
+	}
+
+
+}
